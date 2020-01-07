b42cc16c65cd2493099e01629fb2d2cf632fdd95
Johan-rosa <johan.rosaperez@gmail.com>
2020-01-07 20:00:08 +0000

    Johan Rosa

---
+++ data_cleaning.R
@@ -0,0 +1,14 @@
+# librerias ------------------------------------------------------------------------
+library(tidyverse)
+library(janitor)
+library(leaftlet)
+library(readxl)
+
+# importar archivos ----------------------------------------------------------------
+# set de datos
+centros <- read_excel('centros_educativos_republica_dominicana_2018.xlsx')
+pruebas <- read_excel('resultados_pruebas_nacionales2016_2018.xlsx')
+
+# shape files de la cartografía nacional
+
+# Manipulación de objetos ----------------------------------------------------------
